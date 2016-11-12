896031b8d06a7ff57061ba72cee7507bc5940432
Stephen Farley <i7759889@bournemouth.ac.uk>
2016-11-12 11:34:08 +0000

    added player, updated world

---
+++ SpaceBrewWars/Assets/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+    public int ID;
+    public int money;
+
+	// Use this for initialization
+	void Start () {
+        money = 1000;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void CollectIncome(int income)
+    {
+        money += income;
+    }
+
+    void CollectDebt(int debt)
+    {
+        money -= debt;
+    }
+}
