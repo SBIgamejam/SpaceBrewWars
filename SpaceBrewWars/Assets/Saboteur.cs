73c74161be11314b7576c980c3f347d07ee94f54
Lewis Ward <lewis.ward@outlook.com>
2016-11-12 10:42:29 +0000

    Added Builder, Saboteur and World scripts. Added git ignore file.

---
+++ SpaceBrewWars/Assets/Saboteur.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Saboteur : MonoBehaviour {
+    public Vector3 position; // current position in the world
+    public int owner; // playerID, which player owns it
+    public bool alive; // is this ship alive? if not it need to be cleaned up and removed from the world.
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ SpaceBrewWars/Assets/World.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+    public int worldRadius = 100; // the radius of the world
+    public int locationRadius = 10; // the radius of locations in the world
+    public int selectionRadius = 10; // the radius for selecting buildings and units
+    public int numberOfLevels = 3; // the number of depth levels that world contains
+    //public GameObject camera;
+    //public EntityManager entityManager;
+    //public PathManager pathManager;
+    //public AIManager aiManager;
+    //public UIManager uiManagerl
+    public int numberOfPlayers;
+    int[] players;
+
+	// Use this for initialization
+	void Start () {
+        // make sure the radius and levels are not zero
+        if (worldRadius == 0)
+            worldRadius = 1;
+        if (locationRadius == 0)
+            locationRadius = 1;
+        if (selectionRadius == 0)
+            selectionRadius = 1;
+        if (numberOfLevels == 0)
+            numberOfLevels = 1;
+
+        players = new int[numberOfPlayers];
+
+        WorldGeneration();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+        /* Update managers
+         * INSERT CODE
+         */
+	}
+
+    void WorldGeneration()
+    {
+        // set the size of the transform
+        Transform transform = GetComponent<Transform>();
+        transform.localScale = new Vector3(worldRadius, worldRadius, worldRadius);
+
+        // TODO: call entityCreationManager to create the world and store the results in the EntityManager.
+
+
+    }
+}
