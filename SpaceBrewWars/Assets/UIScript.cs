d4c171512292a7773838d9757c09d79a9364824a
Stephen Farley <i7759889@bournemouth.ac.uk>
2016-11-12 14:15:45 +0000

    pre lunch push

---
+++ SpaceBrewWars/Assets/UIScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public Text Currency;
+    public GameObject world;
+
+    // Use this for initialization
+    void Start () {
+        world = GameObject.FindGameObjectWithTag("World");
+        Currency = GetComponent<Text>();
+        Currency.text = world.GetComponent<World>().players[0].GetComponent<Player>().money.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Currency.text = "hi";
+
+
+    }
+}
