0f10da59099b0469ab32469f854b27f77373c4c9
Stephen Farley <i7759889@bournemouth.ac.uk>
2016-11-12 11:02:01 +0000

    brewary and pub

---
+++ SpaceBrewWars/Assets/Brewery.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Brewery : MonoBehaviour {
+
+    //public System.Collections.Generic.List<Link> links;
+    public int ecoValue;
+    public int owner;
+
+    // Use this for initialization
+    void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+}
